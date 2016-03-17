847bb95371145cd6205e14522917c9739e58c9ba
chenmingzhou <sd@dfh.com>
2016-03-17 14:08:40 +0800

    提交项目

---
+++ src/DvJson.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class DvJson {
+	private Header headers;
+	private String body;
+	public Header getHeaders() {
+		return headers;
+	}
+	public void setHeaders(Header headers) {
+		this.headers = headers;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
---
+++ src/Header.java
@@ -0,0 +1,35 @@
+
+
+public class Header {
+	
+	private String logType;
+	private String gameId;
+	private String sign;
+	private String timestamp;
+	
+	public String getLogType() {
+		return logType;
+	}
+	public void setLogType(String logType) {
+		this.logType = logType;
+	}
+	public String getGameId() {
+		return gameId;
+	}
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
+	public String getSign() {
+		return sign;
+	}
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
